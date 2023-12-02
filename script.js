00ef1c026f7cad8fdd83913eac1fc8fe0434d735
shaikh-yusuf <94888066+shaikh-yusuf@users.noreply.github.com>
2023-12-02 19:28:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,2 @@
 //your JS code here. If required.
-function changeColors(currentSquare) {
-            var squares = document.querySelectorAll('.lavender');
-            for (var i = 0; i < squares.length; i++) {
-                if (squares[i].id !== currentSquare) {
-                    squares[i].classList.add('coffee');
-                }
-            }
-        }
-
-        function restoreColors(currentSquare) {
-            var squares = document.querySelectorAll('.lavender');
-            for (var i = 0; i < squares.length; i++) {
-                if (squares[i].id !== currentSquare) {
-                    squares[i].classList.remove('coffee');
-                }
-            }
-        }
+let 
